f57407d841739e9172fc15751bb9144b9b1c3eb1
Emadstrong <132583034+Emadstrong@users.noreply.github.com>
2024-07-11 01:33:24 +0300

    Update gui.sh

--- gui.sh
+++ gui.sh
@@ -7,6 +7,6 @@ sudo apt install -y tightvncserver libdbus-glib-1-dev libgbm-dev
 vncserver -geometry 800x600 :1
 DISPLAY=:1 export XDG_CURRENT_DESKTOP="GNOME"
 service dbus start
-gnome-shell --x11 > /tmp/lxde.log  & wget -c https://bin.equinox.io/c/bNyj1mQVY4c/ngrok-v3-stable-linux-amd64.tgz -O - | tar -xz
-./ngrok --authtoken (token) tcp 5901
+gnome-shell --x11 & wget -c https://bin.equinox.io/c/bNyj1mQVY4c/ngrok-v3-stable-linux-amd64.tgz -O - | tar -xz
+./ngrok --authtoken token tcp 5901
 echo end
