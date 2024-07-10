0777c4be9b52c2dee78923ca63fa8ed111a9ee74
Emadstrong <132583034+Emadstrong@users.noreply.github.com>
2024-07-11 01:32:05 +0300

    Update gui.sh

--- gui.sh
+++ gui.sh
@@ -1,10 +1,12 @@
 #gui install
 
-sudo apt install lxde vlc pulseaudio neofetch firefox-esr simplescreenrecorder  -y
+sudo apt install gnome-shell gnome-terminal gnome-tweaks gnome-shell-extension-ubuntu-dock nautilus nano gedit dbus-x11 vlc pulseaudio neofetch firefox-esr simplescreenrecorder  -y
 
 #vncinstall
 sudo apt install -y tightvncserver libdbus-glib-1-dev libgbm-dev
 vncserver -geometry 800x600 :1
-DISPLAY=:1 startlxde > /tmp/lxde.log  & wget -c https://bin.equinox.io/c/bNyj1mQVY4c/ngrok-v3-stable-linux-amd64.tgz -O - | tar -xz
+DISPLAY=:1 export XDG_CURRENT_DESKTOP="GNOME"
+service dbus start
+gnome-shell --x11 > /tmp/lxde.log  & wget -c https://bin.equinox.io/c/bNyj1mQVY4c/ngrok-v3-stable-linux-amd64.tgz -O - | tar -xz
 ./ngrok --authtoken (token) tcp 5901
 echo end
